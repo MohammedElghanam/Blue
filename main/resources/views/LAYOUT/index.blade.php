9272a78111b7c3142eeabd7ec57cd08fe3f51b32
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2024-06-04 12:12:24 +0100

    update navbar

--- main/resources/views/LAYOUT/index.blade.php
+++ main/resources/views/LAYOUT/index.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+   
+    <link rel="stylesheet" href="https://fontawesome.com ">
+    <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha384-KyZXEAg3QhqLMpG8r+Knujsl5/3lzH7/g2XHfq+TTb69v59+9E5K3HU9ThUj9gJ+" crossorigin="anonymous"></script>
     <title>@yield('title')</title>
 </head>
