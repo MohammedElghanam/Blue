c88930a9c46de99bfa846c9902633cda72d06db9
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2024-06-06 10:10:09 +0100

    update

--- main/routes/web.php
+++ main/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('HOME.index');
 });
+
+
+Route::get('/A propos de nous', function(){
+    return view('HOME.A_propos');
+})->name('A_propos');
