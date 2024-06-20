4b896fdd92dbfd3c6494fb70d17e55810c7a64c0
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2024-06-20 22:59:14 +0100

    Update A_propos.blade.php

--- main/resources/views/HOME/A_propos.blade.php
+++ main/resources/views/HOME/A_propos.blade.php
@@ -110,11 +110,11 @@
     <div id="FLOAT1"></div>
     <div class="grid max-w-screen-xl py-8 mx-auto gap-20 lg:py-16 lg:grid-cols-12 justify-center ">
 
-        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex justify-center items-center h-full z-10">
+        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex justify-center items-center h-full z-10 bg-red-700">
             <img src="images/team.jpg" alt="mockup" class=" h-full w-full right-0 rounded-lg">
         </div>
 
-        <div class="mr-auto place-self-center lg:col-span-6 z-10">
+        <div class="mr-auto place-self-center lg:col-span-6 z-10 bg-yellow-500">
             <h1 class="max-w-2xl mb-3 xl:text-5xl font-bold  text-gray-800">Description</h1>
 
             <p class="max-w-2xl mb-6 font-medium text-gray-500 lg:mb-8 md:text-lg xl:text-[14px] dark:text-gray-600">BLUE
